a3d1edde3257d3aa5b3d72dff8ea0cfe5f334246
jiyf <ffaa30703@icloud.com>
2020-08-02 14:27:20 +0800

    添加 作业 用 add first 或 add last 这套新的 API 改写 Deque 的代码

---
+++ Week_01/hw1_DequeTest.java
@@ -0,0 +1,28 @@
+package com.keep.java.list;
+
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.*;
+
+
+public class DequeTest {
+
+    public static void main(String[] args) {
+        Deque<String> deque = new LinkedList<String>();
+        deque.addLast("a");
+        deque.addLast("b");
+        deque.addLast("c");
+        System.out.println(deque);
+
+        String str = deque.peekFirst();
+        System.out.println(str);
+        System.out.println(deque);
+        while (deque.size() > 0) {
+            System.out.println(deque.pollFirst());
+        }
+        System.out.println(deque);
+
+    }
+
+}
